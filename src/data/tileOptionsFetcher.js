3ebde26e265ed0e23452e89b4d10418e3520f84c
GILAD BADLI <gabenlovesme1337@gmail.com>
2025-09-24 01:36:39 +0300

    cleaned code

--- src/data/tileOptionsFetcher.js
+++ src/data/tileOptionsFetcher.js
@@ -1,23 +1,33 @@
 import yaml from 'js-yaml';
 
 export async function fetchTileOptions() {
+
+  const isDev = import.meta.env.VITE_DEV;
+  const filePath = isDev ? '../../public/tileOptions.yaml' : '/tileOptions.yaml';
+
+  if (isDev) {
+    console.log(`Development mode: Fetching tile options from ${filePath}`);
+  } else {
+    console.log(`Production mode: Fetching tile options from ${filePath}`);
+  }
+
   try {
-    let filePath= "";
-    if (import.meta.env.DEV) {
-      console.log("Development mode: Fetching tile options from local file.");
-      filePath = '../../public/tileOptions.yaml';
-    } else {
-      console.log("Production mode: Fetching tile options from server.");
-      filePath = '/tileOptions.yaml';
-    }
     const response = await fetch(filePath);
+
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      throw new Error(`HTTP error! Status: ${response.status} - Could not fetch ${filePath}`);
     }
-    const optionsArray = await response.text();
-    return yaml.load(optionsArray).options;
+
+    const yamlText = await response.text();
+    const parsedOptions = yaml.load(yamlText);
+
+    if (!parsedOptions || !parsedOptions.options) {
+      throw new Error('YAML content is not in the expected format.');
+    }
+
+    return parsedOptions.options;
   } catch (error) {
-      console.error("There's a problem with tileOptions.yaml file", error);
-      return null;
+    console.error(`There was a problem with the tileOptions.yaml file:`, error);
+    return null;
   }
 }
