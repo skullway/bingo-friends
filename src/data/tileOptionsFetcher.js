fc374face1a00ba973df0fea349081cb6f23f879
GILAD BADLI <gabenlovesme1337@gmail.com>
2025-09-24 01:24:54 +0300

    fixed fetching bug with DEV/PROD assortment

--- src/data/tileOptionsFetcher.js
+++ src/data/tileOptionsFetcher.js
@@ -1,16 +1,15 @@
 import yaml from 'js-yaml';
 
 export async function fetchTileOptions() {
-    
-  if (import.meta.env.DEV) {
-    console.log("Development mode: Fetching tile options from local file.");
-    const filePath = '../../public/tileOptions.yaml';
-  } else {
-    console.log("Production mode: Fetching tile options from server.");
-    const filePath = '/tileOptions.yaml';
-  }
-
   try {
+    let filePath= "";
+    if (import.meta.env.DEV) {
+      console.log("Development mode: Fetching tile options from local file.");
+      filePath = '../../public/tileOptions.yaml';
+    } else {
+      console.log("Production mode: Fetching tile options from server.");
+      filePath = '/tileOptions.yaml';
+    }
     const response = await fetch(filePath);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
